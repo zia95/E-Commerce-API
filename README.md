e8ca4b4263c5eac2bb46b20ab56a06add4a5037f
ziauddin <ziauddin1234@gmail.com>
2023-10-02 22:41:47 +0500

    setup script and readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# fastapi t1
-this is first
+# E-Commerce Web API
+This is E-Commerece web api demo built using python and fastapi.
+# Setup Instructions
+## 1. Environment variables setup
+Create `.env` file and copy the below content and paste it into it and fill all the environment variables.
+```
+DATABASE_NAME=""
+DATABASE_FILE=""
+HOST=""
+USER=""
+PASSWORD=""
+SECRET_KEY=""
+ACCESS_TOKEN_EXPIRE_MINS=30
+```
+## 2. Environment setup
+### Method A. Using setup script
+Run `setup.ps1` and it will setup all the envionment.
+To run the app run `setup.ps1` script with `-Run` parameter. Like `setup.ps1 -Run`
+### Method B. Manual
+1. Create python virtual environment using `python.exe -m venv <virtual env name>`
+2. Activate virtual environment using `./<virtual env name>/Scripts/Activate.ps1`
+3. Install the dependencies using `python.exe -m pip install -r ./requirements.txt`
+4. To run the api use `uvicorn main:app`
+5. Lastly, open the provided url to locally hosted app and naviagate to `<url>/docs`. This will open swagger docs where you can test the api.
--- setup.ps1
+++ setup.ps1
@@ -1,22 +1,43 @@
-param([string]$VEnvName='venv')
+param([string]$VEnvName='venv', [switch]$Run)
 
-Write-Output '===> Settings up venv...'
-python.exe -m venv $VEnvName
+[int]$ERROR_FAILED_TO_CREATE_VENV = 1
+[int]$ERROR_FAILED_TO_ACTIVATE_VENV = 2
+[int]$ERROR_FAILED_TO_INSTALL_PACKAGES = 3
 
-if(-not $?) { Write-Error "Failed to setup venv..."; exit 1 }
-
-$loc = Get-Location
-
-Set-Location ".\$VEnvName\Scripts"
-./Activate.ps1
-
-if(-not $?) { Write-Error "Failed to activate venv: $VEnvName ..."; exit 2 }
-
-Set-Location $loc
-Write-Output '===> Installing packages....'
-python.exe -m pip install -r .\requirements.txt
-
-if(-not $?) { Write-Error "Failed to install packages..."; exit 3 }
-
-Write-Output '===> Starting webapi srv'
-uvicorn main:app
+if($Run -eq $false)
+{
+    Write-Output '===> Settings up venv...'
+    python.exe -m venv $VEnvName
+    
+    if(-not $?) { Write-Error "Failed to setup venv..."; exit $ERROR_FAILED_TO_CREATE_VENV }
+    
+    
+    $loc = Get-Location
+    
+    Set-Location ".\$VEnvName\Scripts"
+    ./Activate.ps1
+    
+    if(-not $?) { Write-Error "Failed to activate venv: $VEnvName ..."; exit $ERROR_FAILED_TO_ACTIVATE_VENV }
+    
+    Set-Location $loc
+    
+    
+    Write-Output '===> Installing packages....'
+    python.exe -m pip install -r .\requirements.txt
+    
+    if(-not $?) { Write-Error "Failed to install packages..."; exit $ERROR_FAILED_TO_INSTALL_PACKAGES }
+}
+else 
+{
+    $loc = Get-Location
+    
+    Set-Location ".\$VEnvName\Scripts"
+    ./Activate.ps1
+    
+    if(-not $?) { Write-Error "Failed to activate venv: $VEnvName ..."; exit $ERROR_FAILED_TO_ACTIVATE_VENV }
+    
+    Set-Location $loc
+    
+    Write-Output '===> Starting webapi srv'
+    uvicorn main:app
+}
